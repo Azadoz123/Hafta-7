e160e37e2a696d9d7094fe6bded0bdc4a2106d0c
Azadoz123 <azad.ozkaynak@gmail.com>
2024-01-27 22:40:07 +0300

    Model

---
+++ RentACar/src/dao/BrandDao.java
@@ -0,0 +1,40 @@
+package dao;
+
+import core.Db;
+import entity.Brand;
+import entity.User;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class BrandDao {
+    private final Connection connection;
+
+    public BrandDao() {
+        this.connection = Db.getInstance();
+    }
+
+    public ArrayList<Brand> findAll(){
+        ArrayList<Brand> brandList = new ArrayList<>();
+        String sql = "SELECT * FROM public.brand";
+        Brand user = null;
+        try {
+            ResultSet resultSet = this.connection.createStatement().executeQuery(sql);
+            while (resultSet.next()){
+                brandList.add(this.match(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return brandList;
+    }
+    public Brand match(ResultSet resultSet) throws SQLException {
+        Brand brand = new Brand();
+        brand.setId(resultSet.getInt("brand_id"));
+        brand.setName(resultSet.getString("brand_name"));
+
+        return brand;
+    }
+}
--- RentACar/src/view/AdminView.java
+++ RentACar/src/view/AdminView.java
@@ -1,8 +1,15 @@
 package view;
 
+import business.BrandManager;
+import entity.Brand;
 import entity.User;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 public class AdminView extends Layout{
     private JPanel container;
@@ -13,8 +20,12 @@ public class AdminView extends Layout{
     private JPanel pnl_brand;
     private JTable tbl_brand;
     private User user;
+    private DefaultTableModel t_mdl_brand = new DefaultTableModel();
+    private JPopupMenu brandMenu;
 
+    private BrandManager brandManager;
     public AdminView(User user) {
+        this.brandManager = new BrandManager();
         this.add(container);
         guiInitialize(1000,500);
         this.user = user;
@@ -23,5 +34,34 @@ public class AdminView extends Layout{
         }
 
         this.lbl_welcome.setText("Hoşgeldiniz " + this.user.getUsername());
+
+        Object[] col_brand = {"Marka ID", "Marka Adı"};
+        ArrayList<Brand> brandArrayList = brandManager.findAll();
+        this.t_mdl_brand.setColumnIdentifiers(col_brand);
+        for (Brand brand: brandArrayList){
+            Object[] obj = {brand.getId(), brand.getName()};
+            t_mdl_brand.addRow(obj);
+        }
+
+        this.tbl_brand.setModel(t_mdl_brand);
+        this.tbl_brand.getTableHeader().setReorderingAllowed(false);
+        this.tbl_brand.setEnabled(false);
+        this.tbl_brand.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int selectedRow = tbl_brand.rowAtPoint(e.getPoint());
+                tbl_brand.setRowSelectionInterval(selectedRow,selectedRow);
+
+            }
+        });
+
+        this.brandMenu = new JPopupMenu();
+        this.brandMenu.add("Yeni").addActionListener(e -> {
+            System.out.println("Yeni butonu tıklandı");
+        });
+        this.brandMenu.add("Güncelle");
+        this.brandMenu.add("Sil");
+
+        this.tbl_brand.setComponentPopupMenu(brandMenu);
     }
 }
