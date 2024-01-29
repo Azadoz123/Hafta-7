1fd9ba13dc542827e7323b67965a985337520a53
Azadoz123 <azad.ozkaynak@gmail.com>
2024-01-29 17:21:21 +0300

    model 2

--- RentACar/src/business/ModelManager.java
+++ RentACar/src/business/ModelManager.java
@@ -13,4 +13,20 @@ public class ModelManager {
     public ArrayList<Model> findAll(){
         return this.modelDao.findAll();
     }
+    public ArrayList<Object[]> getForTable(int size, ArrayList<Model> modelList){
+        ArrayList<Object[]> modelObjList = new ArrayList<>();
+        for (Model model : modelList){
+            int i = 0;
+            Object[] rowObject = new Object[size];
+            rowObject[i++] = model.getId();
+            rowObject[i++] = model.getBrand().getName();
+            rowObject[i++] = model.getName();
+            rowObject[i++] = model.getType();
+            rowObject[i++] = model.getYear();
+            rowObject[i++] = model.getFuel();
+            rowObject[i++] = model.getGear();
+            modelObjList.add(rowObject);
+        }
+        return modelObjList;
+    }
 }
